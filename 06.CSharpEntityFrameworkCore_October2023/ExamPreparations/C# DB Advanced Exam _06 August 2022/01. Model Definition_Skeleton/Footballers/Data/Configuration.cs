cf29080c11d118e1150da7140c30ee1b6cf8c7e5
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-10-31 18:53:12 +0200

    commit

--- 06.CSharpEntityFrameworkCore_October2023/ExamPreparations/C# DB Advanced Exam _06 August 2022/01. Model Definition_Skeleton/Footballers/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Footballers.Data
-{
-    public static class Configuration
-    {
-        public static string ConnectionString =
-            @"Server=DESKTOP-ME8RHIA;Database=FootballersExam;Trusted_Connection=True";
-    }
-}
---
+++ 07.ASP.NET - Fundamentals_January2024/01.ASP.NET Core Introduction/ChatApp/Models/Message/MessageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Models.Message
+{
+    public class MessageViewModel
+    {
+        [Display(Name = "Sender Name")]
+        [Required(ErrorMessage = "Field {0} is required!")]
+        public string Sender { get; set; } = null!;
+
+        [Display(Name = "Message")]
+        [Required(ErrorMessage = "Field {0} is required!")]
+        public string MessageText { get; set; } = null!;
+    }
+}
